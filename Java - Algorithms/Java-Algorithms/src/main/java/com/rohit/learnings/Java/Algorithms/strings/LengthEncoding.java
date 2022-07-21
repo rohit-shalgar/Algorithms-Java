99b5bf73339a9da2b0469c53fd9eb376a487f404
rohit-shalgar <learn.rohits@gmail.com>
2022-07-20 20:19:16 -0500

    Length encoder string

--- Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/strings/LengthEncoding.java
+++ Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/strings/LengthEncoding.java
@@ -7,8 +7,7 @@ public class LengthEncoding {
         for (int i = 1; i < input.length(); i++) {
             char currentCharacter = input.charAt(i);
             char previousCharacter = input.charAt(i - 1);
-            if (currentCharacter != previousCharacter
-                    || count == 9) {
+            if (currentCharacter != previousCharacter || count == 9) {
                 encodedStringBuilder.append(count);
                 encodedStringBuilder.append(previousCharacter);
                 count = 0;
