7d0c5006cbe8042c1605f19859ea4b0f20dc58ef
rohit-shalgar <learn.rohits@gmail.com>
2022-08-06 17:50:35 -0500

    non constructible change

--- Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/arrays/NonConstructibleChange.java
+++ Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/arrays/NonConstructibleChange.java
@@ -4,16 +4,13 @@ import java.util.Arrays;
 
 public class NonConstructibleChange {
     public static int getChange(int[] input) {
-        if (input.length == 0) return 1;
         int change = 0;
-        int[] sortedInput = input;
-        Arrays.sort(sortedInput);
-        for (int i = 0; i < sortedInput.length; i++) {
-            int current = sortedInput[i];
-            if (current > change + 1) {
+        Arrays.sort(input);
+        for (int element : input) {
+            if (element > change + 1) {
                 return change + 1;
             }
-            change = change + current;
+            change = change + element;
         }
         return change + 1;
     }
