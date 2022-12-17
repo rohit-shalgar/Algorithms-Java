bc59b38fb364b7ee8fc680eee8e66e864e271ea3
rohit-shalgar <learn.rohits@gmail.com>
2022-12-17 14:30:53 -0600

    Optimized kth highest value BST

--- Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/BST/KthHighestValue.java
+++ Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/learnings/Java/Algorithms/BST/KthHighestValue.java
@@ -14,10 +14,36 @@ public class KthHighestValue {
         }
     }
 
+    static class TreeInfo {
+        int noOfNodesVisited;
+        int lastVisitedNodeValue;
+
+        public TreeInfo(int noOfNodesVisited, int lastVisitedNodeValue) {
+            this.noOfNodesVisited = noOfNodesVisited;
+            this.lastVisitedNodeValue = lastVisitedNodeValue;
+        }
+    }
+
     public int findKthLargestValueInBst(BST tree, int k) {
         List<Integer> inOrderTree = new ArrayList<>();
         inOrderTraversalHelper(tree, inOrderTree);
         return inOrderTree.get(k - 1);
+    }
+
+    public int findKthLargestValueInBstOpt(BST tree, int k) {
+        TreeInfo treeInfo = new TreeInfo(0, -1);
+        inOrderTraversalHelper(tree, treeInfo, k);
+        return treeInfo.lastVisitedNodeValue;
+    }
+
+    private void inOrderTraversalHelper(BST tree, TreeInfo treeInfo, int k) {
+        if (tree == null || treeInfo.noOfNodesVisited >= k) return;
+        inOrderTraversalHelper(tree.right, treeInfo, k);
+        if (treeInfo.noOfNodesVisited < k) {
+            treeInfo.noOfNodesVisited += 1;
+            treeInfo.lastVisitedNodeValue = tree.value;
+        }
+        inOrderTraversalHelper(tree.left, treeInfo, k);
     }
 
     private void inOrderTraversalHelper(BST tree, List<Integer> inOrderTree) {
