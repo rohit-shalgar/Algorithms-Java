b2fbd920d564d71186af114a34d7501ec5a4cc74
shalgar,rohit rajkumar <rshalgar@itron.com>
2024-06-07 12:54:02 -0600

    No of ways to top with sliding window

--- Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/twoTwentyFour/recurssion/Programs.java
+++ Java - Algorithms/Java-Algorithms/src/main/java/com/rohit/twoTwentyFour/recurssion/Programs.java
@@ -1,6 +1,8 @@
 package com.rohit.twoTwentyFour.recurssion;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Programs {
@@ -37,4 +39,36 @@ public class Programs {
         memoize.put(currentHeight,noOfWaysToTop);
         return noOfWaysToTop;
     }
+
+    public int staircaseTraversalDynamic(int height, int maxSteps) {
+        int[] noOfWaysToEachStep = new int[height + 1];
+        noOfWaysToEachStep[0] = 1;
+        noOfWaysToEachStep[1] = 1;
+        for(int i = 2; i <= height; i ++){
+            int currentWays = 0;
+            for(int step = 1; step <= Math.min(i,maxSteps); step++){
+                currentWays += noOfWaysToEachStep[i -step];
+            }
+            noOfWaysToEachStep[i] = currentWays;
+        }
+        return noOfWaysToEachStep[height];
+    }
+
+    public int staircaseTraversalSlidingWindow(int height, int maxSteps) {
+        int currentNoOfWays = 0;
+        List<Integer> waysToTop = new ArrayList<>();
+        waysToTop.add(1);
+        for(int currentHeight = 1; currentHeight <= height; currentHeight++){
+            int startOfTheWindow = currentHeight - maxSteps - 1;
+            int endOfTheWindow = currentHeight - 1;
+            if(startOfTheWindow >= 0){
+                currentNoOfWays -= waysToTop.get(startOfTheWindow);
+            }
+            currentNoOfWays += waysToTop.get(endOfTheWindow);
+            waysToTop.add(currentNoOfWays);
+
+        }
+        return waysToTop.get(height);
+    }
+
 }
